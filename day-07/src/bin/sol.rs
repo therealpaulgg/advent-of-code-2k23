0ccb5dbf33f3ba4d474ecb7ee5c1eac3f837cb6b
therealpaulgg <paul@paul.systems>
2023-12-09 00:49:57 -0700

    completely misinterpreted

--- day-07/src/bin/sol.rs
+++ day-07/src/bin/sol.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 use std::cmp::Ordering;
-use std::collections::HashMap;
 use std::collections::BTreeMap;
+use std::collections::HashMap;
 
 fn main() {
     let input = include_str!("./input.txt");
@@ -13,19 +13,19 @@ fn main() {
 
 fn part1(input: &str) -> String {
     let card_value_mapping = HashMap::from([
-        ('2', 1),
-        ('3', 2),
-        ('4', 3),
-        ('5', 4),
-        ('6', 5),
-        ('7', 6),
-        ('8', 7),
-        ('9', 8),
-        ('T', 9),
-        ('J', 10),
-        ('Q', 11),
-        ('K', 12),
-        ('A', 13),
+        ('2', 2),
+        ('3', 3),
+        ('4', 4),
+        ('5', 5),
+        ('6', 6),
+        ('7', 7),
+        ('8', 8),
+        ('9', 9),
+        ('T', 10),
+        ('J', 11),
+        ('Q', 12),
+        ('K', 13),
+        ('A', 14),
     ]);
     let cap = Regex::new(r"(\w+) (\d+)").unwrap();
     let hands = input.lines().map(|line| {
@@ -35,33 +35,31 @@ fn part1(input: &str) -> String {
         let hand_type = classify_hand(hand);
         (hand_type, hand, num)
     });
-    let col = hands.clone().collect::<Vec<_>>();
     // map by hand type
-    let mut hand_map: BTreeMap<Hand, Vec<((Hand, char), &str, usize)>> = BTreeMap::new();
+    let mut hand_map: BTreeMap<Hand, Vec<(Hand, &str, usize)>> = BTreeMap::new();
     hands.clone().for_each(|hand| {
         let (hand_type, hand, num) = hand;
-        let entry = hand_map.entry(hand_type.0).or_insert(Vec::new());
+        let entry = hand_map.entry(hand_type).or_insert(Vec::new());
         entry.push((hand_type, hand, num));
     });
     // sort hand_map by hand type
     let mut stuff: Vec<_> = hand_map.clone().into_iter().map(|score| score).collect();
     stuff.sort_by_key(|f| f.0);
-    let mut all_vals: Vec<((Hand, char), &str, usize)> = Vec::new();
+    let mut all_vals: Vec<(Hand, &str, usize)> = Vec::new();
     for mut thing in hand_map.clone() {
         thing.1.sort_by(|a, b| {
-            let a_val = *card_value_mapping.get(&a.0.1).unwrap();
-            let b_val = *card_value_mapping.get(&b.0.1).unwrap();
-            if a_val == b_val {
-                for i in 0..5 {
-                    let char_a = a.1.chars().nth(i).unwrap();
-                    let char_b = b.1.chars().nth(i).unwrap();
-                    let cmp = card_value_mapping.get(&char_a).unwrap().cmp(card_value_mapping.get(&char_b).unwrap());
-                    if cmp != Ordering::Equal {
-                        return cmp
-                    }
+            for i in 0..5 {
+                let char_a = a.1.chars().nth(i).unwrap();
+                let char_b = b.1.chars().nth(i).unwrap();
+                let cmp = card_value_mapping
+                    .get(&char_a)
+                    .unwrap()
+                    .cmp(card_value_mapping.get(&char_b).unwrap());
+                if cmp != Ordering::Equal {
+                    return cmp;
                 }
             }
-            return a_val.cmp(&b_val)
+            Ordering::Equal
         });
         all_vals.append(&mut thing.1)
     }
@@ -87,80 +85,51 @@ enum Hand {
     FiveOfAKind,
 }
 
-fn classify_hand(hand: &str) -> (Hand, char) {
+fn classify_hand(hand: &str) -> Hand {
     // Hand contains one of A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, or 2
     // every hand has 5 characters.
     // check if all characters in the hand are the same
 
     if hand.chars().all(|c| c == hand.chars().next().unwrap()) {
-        return (Hand::FiveOfAKind, hand.chars().next().unwrap());
+        return Hand::FiveOfAKind;
     }
-    let mut handMap = HashMap::new();
+    let mut hand_map = HashMap::new();
     for c in hand.chars() {
-        let count = handMap.entry(c).or_insert(0);
+        let count = hand_map.entry(c).or_insert(0);
         *count += 1;
     }
-    if handMap.keys().len() == 2 {
+    if hand_map.keys().len() == 2 {
         // can be 4ofakind or fullhouse
-        let keys: Vec<_> = handMap.keys().collect();
-        let four_of_a_kind_key = keys.iter().find(|&&k| handMap.get(k) == Some(&4));
+        let keys: Vec<_> = hand_map.keys().collect();
+        let four_of_a_kind_key = keys.iter().find(|&&k| hand_map.get(k) == Some(&4));
         if four_of_a_kind_key.is_some() {
-            return (Hand::FourOfAKind, **four_of_a_kind_key.unwrap());
+            return Hand::FourOfAKind;
         }
-        let stronger = get_strongest_card(keys);
-        return (Hand::FullHouse, *stronger);
+        return Hand::FullHouse;
     }
-    if handMap.keys().len() == 3 {
+    if hand_map.keys().len() == 3 {
         // can be 3ofakind or twopairs
-        let keys: Vec<_> = handMap.keys().collect();
-        let three_of_a_kind_key = keys.iter().find(|&&k| handMap.get(k) == Some(&3));
+        let keys: Vec<_> = hand_map.keys().collect();
+        let three_of_a_kind_key = keys.iter().find(|&&k| hand_map.get(k) == Some(&3));
         if three_of_a_kind_key.is_some() {
-            return (Hand::ThreeOfAKind, **three_of_a_kind_key.unwrap());
+            return Hand::ThreeOfAKind;
         }
         for i in 0..3 {
             for j in i + 1..3 {
-                if handMap.get(keys[i]) == Some(&2) && handMap.get(keys[j]) == Some(&2) {
-                    return (Hand::TwoPairs, *get_strongest_card(vec![keys[i], keys[j]]));
+                if hand_map.get(keys[i]) == Some(&2) && hand_map.get(keys[j]) == Some(&2) {
+                    return Hand::TwoPairs;
                 }
             }
         }
     }
-    if handMap.keys().len() == 4 {
-        for key in handMap.keys() {
-            if handMap.get(key) == Some(&2) {
-                return (Hand::OnePair, *key);
+    if hand_map.keys().len() == 4 {
+        for key in hand_map.keys() {
+            if hand_map.get(key) == Some(&2) {
+                return Hand::OnePair;
             }
         }
     }
-    (
-        Hand::HighCard,
-        *get_strongest_card(handMap.keys().collect()),
-    )
-}
-
-fn get_strongest_card(cards: Vec<&char>) -> &char {
-    let card_value_mapping = HashMap::from([
-        ('2', 1),
-        ('3', 2),
-        ('4', 3),
-        ('5', 4),
-        ('6', 5),
-        ('7', 6),
-        ('8', 7),
-        ('9', 8),
-        ('T', 9),
-        ('J', 10),
-        ('Q', 11),
-        ('K', 12),
-        ('A', 13),
-    ]);
-    let mut strongest = cards[0];
-    for card in cards {
-        if card_value_mapping.get(&card).unwrap() > card_value_mapping.get(&strongest).unwrap() {
-            strongest = card;
-        }
-    }
-    strongest
+    Hand::HighCard
 }
 
 #[cfg(test)]
@@ -188,42 +157,42 @@ QQQJA 483";
     #[test]
     fn five_of_a_kind() {
         let result = classify_hand("22222");
-        assert_eq!(result, (Hand::FiveOfAKind, '2'));
+        assert_eq!(result, Hand::FiveOfAKind);
     }
 
     #[test]
     fn four_of_a_kind() {
         let result = classify_hand("22223");
-        assert_eq!(result, (Hand::FourOfAKind, '2'));
+        assert_eq!(result, Hand::FourOfAKind);
     }
 
     #[test]
     fn full_house() {
         let result = classify_hand("22233");
-        assert_eq!(result, (Hand::FullHouse, '3'));
+        assert_eq!(result, Hand::FullHouse);
     }
 
     #[test]
     fn three_of_a_kind() {
         let result = classify_hand("22234");
-        assert_eq!(result, (Hand::ThreeOfAKind, '2'));
+        assert_eq!(result, Hand::ThreeOfAKind);
     }
 
     #[test]
     fn two_pairs() {
         let result = classify_hand("22544");
-        assert_eq!(result, (Hand::TwoPairs, '4'));
+        assert_eq!(result, Hand::TwoPairs);
     }
 
     #[test]
     fn one_pair() {
         let result = classify_hand("22345");
-        assert_eq!(result, (Hand::OnePair, '2'));
+        assert_eq!(result, Hand::OnePair);
     }
 
     #[test]
     fn high_card() {
         let result = classify_hand("23456");
-        assert_eq!(result, (Hand::HighCard, '6'));
+        assert_eq!(result, Hand::HighCard);
     }
 }
